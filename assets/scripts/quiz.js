4682b8e187527fbdad99e950248d5b155633b7e1
cwallacebailey <cwallacebailey@gmail.com>
2022-02-01 17:40:22 +0000

    create instructions() function

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -3,3 +3,21 @@
 function focus() {
     document.getElementById("username").focus();
   }
+
+// Opens instruction box on being clicked
+
+function instructions() {
+    let instructionsBox = document.getElementById("instructionsBox");
+    instructionsBox.setAttribute("style", "display: block");
+    let instructions = document.getElementById("instructions");
+    instructions.setAttribute("style", "display: none");
+}
+
+// Closes instruction box on being clicked
+
+function instructionsGone() {
+    let instructionsBox = document.getElementById("instructionsBox");
+    instructionsBox.setAttribute("style", "display: none");
+    let instructions = document.getElementById("instructions");
+    instructions.setAttribute("style", "display: block");
+}
